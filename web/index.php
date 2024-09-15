3593fcb59111b6de9a4c26f6509c6f5e9a35fda6
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2024-09-15 22:30:08 +0100

    Update

--- web/index.php
+++ web/index.php
@@ -1,10 +1,26 @@
-<title>DoDS Stats</title>
-<link rel="stylesheet" href="./assets/css/tailwind.output.css" />
-
 <?php
 
 // vars
 $player_rank = 1;
+
+
+function secondsToHoursMinutes($seconds) { 
+      
+    // Calculate the hours 
+    $hours = floor($seconds / 3600); 
+  
+    // Calculate the remaining seconds 
+    // into minutes 
+    $minutes = floor(($seconds % 3600) / 60); 
+  
+    // Return the result as an  
+    // associative array 
+    return [ 
+        'hours'   => $hours, 
+        'minutes' => $minutes, 
+    ]; 
+} 
+
 
 include 'database.php';
 
@@ -15,68 +31,60 @@ $conn = new mysqli($servername,$username,$password,$database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully<br>";
+// echo "Connected successfully<br>";
 
-
-
-$sql = "SELECT name, steamid, score FROM dodstats ORDER BY score LIMIT 10";
+$sql = "SELECT name, steamid, score, timeplayed, kills, deaths FROM dodstats ORDER BY score LIMIT 10";
 $result = $conn->query($sql);
+
+?>
+<html>
+<title>DoDS Stats</title>
+<link rel="stylesheet" href="./assets/css/tailwind.output.css" />
+
+<body>
+<h4>DoDS Stats</h4>
+<table>
+<thead>
+    <tr>
+        <th>Rank</th>
+        <th>Name</th>
+        <th>Points</th>
+        <th>Time</th>
+        <th>kpd</th>
+    </tr>
+</thead>
+<tbody>
+
+<?php
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "Rank: " . $player_rank++ . " Name: " . $row["name"]. " - Steamid: " . $row["steamid"]. " - Score " . $row["score"]. "<br>";
-  }
+    // echo "Rank: " . $player_rank++ . " Name: " . $row["name"]. " - Steamid: " . $row["steamid"]. " - Points " . $row["score"]. "<br>";
+
+$duration = secondsToHoursMinutes($row["timeplayed"]);
+
+$timeplayed_neat = "{$duration['hours']}:{$duration['minutes']}h"; 
+
+
+
+?>
+    <tr>
+        <td><?php echo $player_rank++ ?></td>
+        <td><?php echo $row["name"] ?></td>
+        <td><?php echo $row["score"] ?></td>
+        <td><?php echo $timeplayed_neat ?></td>
+        <td><?php echo (round($row["kills"]/$row["deaths"],1)) ?></td>
+    </tr>
+<?php
+                }
 } else {
   echo "0 results";
 }
 $conn->close();
 
-
-
-
-
 ?>
-
-
-
-            <!-- With avatar -->
-            <h4
-              class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
-            >
-              DoDS Stats
-            </h4>
-            <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
-              <div class="w-full overflow-x-auto">
-                <table class="w-full whitespace-no-wrap">
-                  <thead>
-                    <tr
-                      class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
-                    >
-                      <th class="px-4 py-3">Rank</th>
-                      <th class="px-4 py-3">Name</th>
-                      <th class="px-4 py-3">Points</th>
-                    </tr>
-                  </thead>
-                  <tbody
-                    class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
-                  >
-                    <tr class="text-gray-700 dark:text-gray-400">
-                      <td class="px-4 py-3">
-                            <p class="font-semibold"><?php echo $player_rank++ ?></p>
-                        </div>
-                      </td>
-                      <td class="px-4 py-3 text-sm">
-                        <?php echo $player_rank++ ?>
-                      </td>
-                      <td class="px-4 py-3 text-xs">
-                        <?php echo $player_rank++ ?>
-                    </tr>
-
-
-                    </tbody>
-                </table>
-              </div>
-              <div
-                class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
-              >
+</tbody>
+</table>
+</body>
+</html>
